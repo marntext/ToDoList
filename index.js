af5ec7b4e9baff643ec382a0d4eb862f865e713a
marntext <marntext@gmail.com>
2021-06-13 14:54:36 +0300

    Added addTodo and doneTodo functions

--- index.js
+++ index.js
@@ -4,5 +4,17 @@ const todoListElement = document.querySelector("#myUL");
 document.querySelector("#todo_button").addEventListener("click", addTodo)
 function addTodo() {
     const todoText = document.querySelector("#myInput").value;
+    const todoObject = {
+        id: todoList.length,
+        todoText: todoText,
+        isDone: false,
+    }
     console.log(todoText);
+    todoList.push(todoObject);
+
+}
+
+function doneTodo(todoId) {
+    const selectedTodoIndex =todoList.findIndex((myTodo) => myTodo.id == todoId);
+    todoList[selectedTodoIndex].isDone =true;
 }
